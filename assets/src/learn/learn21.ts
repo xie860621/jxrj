6acb566612fafa45ca022608cc0155234a673ba1
xpl <xie860621@gmail.com>
2023-11-30 09:22:28 +0800

    新增：learn21

--- assets/src/learn/learn21.ts
+++ assets/src/learn/learn21.ts
@@ -25,17 +25,23 @@ export default class Learn21 extends LearnBase {
     @property(cc.Node)
     pointP: cc.Node;
 
+    @property(cc.Node)
+    pointE: cc.Node;
+
     @property(cc.Slider)
     slider: cc.Slider;
 
     @property(cc.Node)
     rotationNode: cc.Node;
 
-    @property(cc.Node)
-    addNode: cc.Node;
+    a: number = 320;
+    b: number = 160;
+
+    moveNode: cc.Node;
 
 
     init () {
+        this.moveNode = this.node.getChildByName("moveNode");
         this.updatePpoint(this.getNodeWorldPosition(this.pointP))
 
         // cc.Vec3.add(_tempVec3, this.triangle.B.position, this.triangle.C.position)
@@ -48,6 +54,8 @@ export default class Learn21 extends LearnBase {
         position.y = pPosition.y;
         position.x = cc.misc.clampf(position.x, this.rect4.A.convertToWorldSpaceAR(cc.Vec3.ZERO).x, this.rect4.B.convertToWorldSpaceAR(cc.Vec3.ZERO).x);
         this.pointP.position = this.rect1.node.convertToNodeSpaceAR(position);
+
+        this.b = this.pointP.position.x + 160;
 
         _tempVec3.x = this.rect1.C.position.x;
         _tempVec3.y = this.pointP.position.x;
@@ -63,15 +71,19 @@ export default class Learn21 extends LearnBase {
 
 
         _tempVec3.x = this.rect1.B.position.x - 160;
-        _tempVec3.y = this.rect5.A.position.y;
+        _tempVec3.y = this.rect1.A.position.y - this.b + 160;
         this.rect5.A.position = _tempVec3;
 
+        _tempVec3.x = this.rect5.B.position.x;
+        _tempVec3.y = this.rect1.A.position.y - this.b + 160;
+        this.rect5.B.position = _tempVec3;
+
         _tempVec3.x = this.rect5.D.position.x;
-        _tempVec3.y = this.rect1.C.position.y;
+        _tempVec3.y = this.rect1.C.position.y - this.b + 160;
         this.rect5.D.position = _tempVec3;
 
         _tempVec3.x = this.rect5.A.position.x;
-        _tempVec3.y = this.rect5.D.position.y;
+        _tempVec3.y = this.rect1.C.position.y - this.b + 160;
         this.rect5.C.position = _tempVec3;
 
         this.onSlider();
@@ -88,35 +100,22 @@ export default class Learn21 extends LearnBase {
 
 
     updateDraw (drawHelper: DrawHelper): void {
-        let nodes = [this.rect2.C, this.rect3.C, this.rect3.D, this.rect5.D]
+        let nodes = [this.rect2.C, this.rect3.C, this.rect3.D, this.pointE]
         let vertexs = [];
         nodes.forEach((node) => {
             vertexs.push(node.convertToWorldSpaceAR(cc.Vec2.ZERO));
         });
         drawHelper.fillVertexs(vertexs, this.rect2.fillColor);
-        drawHelper.drawVertexs(vertexs, false, false);
+        drawHelper.drawVertexs(vertexs, false, true);
     }
 
     onSlider () {
-        let moveNode = this.pointP.parent.parent;
-        this.rect5.node.parent = moveNode;
-        this.rect5.node.position = new cc.Vec3(460, 0);
-
-        this.rotationNode.parent = this.rect5.node;
-
-        // 需要更新重点坐标
-        this.rotationNode.position = new cc.Vec3(this.rect5.A.position.x + 80, this.rect5.C.position.y / 2, 0);
-
-        let pos = this.getNodeWorldPosition(this.rotationNode)
-
-        this.rotationNode.parent = moveNode;
-        this.rotationNode.position = moveNode.convertToNodeSpaceAR(new cc.Vec3(pos.x, pos.y));
-
-        this.rect5.node.parent = this.rotationNode;
-        this.rect5.node.position = new cc.Vec3(80, 80);
-
+        // this.rect5.node.parent = this.rotationNode;
+        // this.rect5.node.position = new cc.Vec3(0, 0)
         this.rotationNode.rotation = this.slider.progress * 90;
-        //  this.addNode.position = cc.v3(460 + this.slider.progress * (320 - (this.pointP.position.x - this.rect1.A.position.x)), this.slider.progress * 160 - 160 + this.slider.progress * this.rect1.D.position.y);
+
+        this.rotationNode.position = new cc.Vec3(460 + this.b * this.slider.progress, this.b - 160, 0);
+
         this.sendUpdateDraw();
     }
 
